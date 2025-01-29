5ca5ebdff1a3962847c2c48775577bec5dbf6c6f
Christian Armstrong <christian_armstrong@brown.edu>
2025-01-29 12:54:57 -0500

    Updated Informed Search lecture slides+notes

--- src/Components/Lectures/Lectures.tsx
+++ src/Components/Lectures/Lectures.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { TableSection } from "../TableSection/TableSection";
-import { RowOptions } from "../TableSection/TableSection";
+import { RowOptions, TableSection } from "../TableSection/TableSection";
 
 interface LectureProps {}
 
@@ -10,39 +9,75 @@ interface LectureProps {}
  * @return true if "now" is after/on the release date, false otherwise
  */
 function shouldRelease(assignment: RowOptions): boolean {
-    if (assignment.autoReleaseDate === undefined) {
-      return true; // If no defined release date, treat it as released if it has an href
-    }
-  
-    const now = new Date();
-  
-    // Convert now into EST, if it isn't already
-    const nowEST = new Date(
-      now.toLocaleString("en-US", {
-        timeZone: "America/New_York",
-      })
-    );
-  
-    return nowEST > new Date(assignment.autoReleaseDate);
+	if (assignment.autoReleaseDate === undefined) {
+		return true; // If no defined release date, treat it as released if it has an href
+	}
+
+	const now = new Date();
+
+	// Convert now into EST, if it isn't already
+	const nowEST = new Date(
+		now.toLocaleString("en-US", {
+			timeZone: "America/New_York",
+		})
+	);
+
+	return nowEST > new Date(assignment.autoReleaseDate);
 }
 
 export class Lectures extends React.Component<LectureProps> {
-    
-    myHeader: string[] = [
-        "Date",
-        "Topic",
-        "Readings",
-        "Notes",
-        "Additional Resources",
-    ];
+	myHeader: string[] = [
+		"Date",
+		"Topic",
+		"Readings",
+		"Notes",
+		"Additional Resources",
+	];
 
+	myRows: RowOptions[] = [
+		{
+			cellNames: ["1/22", "Introduction to AI", "R&N Chapter 1", "Slides", " "],
+			cellNametoLinks: new Map<string, string>([
+				[
+					"Slides",
+					"https://docs.google.com/presentation/d/1xpg5aYXuw2Psp1nxxzDkSOsmMQijVjdgAa2Z1oMk3Fg/edit?usp=sharing",
+				],
+			]),
+			autoReleaseDate: "Jan 22, 2025 02:00:00 EDT",
+		},
+		{
+			cellNames: [
+				"1/24",
+				"Blind Search",
+				"R&N 3.0-3.4",
+				"Notes",
+				"Blind Search Algorithms",
+			],
+			cellNametoLinks: new Map<string, string>([
+				["Notes", require("./lecture_notes/blind_search.pdf")],
+				[
+					"Blind Search Algorithms",
+					"https://cs.stanford.edu/people/eroberts/courses/soco/projects/2003-04/intelligent-search/blind.html",
+				],
+			]),
+			autoReleaseDate: "Jan 24, 2025 02:00:00 EDT",
+		},
+		{
+			cellNames: [
+				"1/27",
+				"Informed Search",
+				"R&N 3.4-3.5.1",
+				"Notes",
+				"Slides",
+			],
+			cellNametoLinks: new Map<string, string>([
+				["Notes", require("./lecture_notes/informed_search.pdf")],
+				["Slides", require("./slides/A-star Search.pdf")],
+			]),
+			autoReleaseDate: "Jan 29, 2025 02:00:00 EDT",
+		},
 
-    myRows: RowOptions[] = [
-        { cellNames: ["1/22", "Introduction to AI", "R&N Chapter 1", "Slides", " "], cellNametoLinks: new Map<string,string>([["Slides", "https://docs.google.com/presentation/d/1xpg5aYXuw2Psp1nxxzDkSOsmMQijVjdgAa2Z1oMk3Fg/edit?usp=sharing"]]),autoReleaseDate: "Jan 22, 2025 02:00:00 EDT" },
-        { cellNames: ["1/24", "Blind Search", "R&N 3.0-3.4", "Notes", "Blind Search Algorithms"], cellNametoLinks: new Map<string,string>([["Notes", require("./lecture_notes/blind_search.pdf")], ["Blind Search Algorithms", "https://cs.stanford.edu/people/eroberts/courses/soco/projects/2003-04/intelligent-search/blind.html"]]),autoReleaseDate: "Jan 24, 2025 02:00:00 EDT"  },
-        
-        /**
-        { cellNames: ["1/27", "Informed Search", "R&N 3.4-3.5.1", "Notes", "Heuristic Search Algorithms"], cellNametoLinks: new Map<string,string>([["Notes", "./lecture_notes/informed_search.pdf"], ["Heuristic Search Algorithms", "https://cs.stanford.edu/people/eroberts/courses/soco/projects/2003-04/intelligent-search/blind.html"]]),autoReleaseDate: "Mar 16, 2025 04:00:00 EDT"  },
+		/**
         { cellNames: ["1/29", "A* Search", "R&N 3.5.2-3.7", "Notes", "Pathfinding Visualizations"], cellNametoLinks: new Map<string,string>([["Notes", "lecture_notes/informed_search.pdf"],["Pathfinding Visualizations", "https://qiao.github.io/PathFinding.js/visual/"]]),autoReleaseDate: "Mar 16, 2025 04:00:00 EDT"  },
         { cellNames: ["1/31", "Adversarial Search", "R&N 5.0-5.3", "Notes", " "], cellNametoLinks: new Map<string,string>([["Notes", "lecture_notes/adversarial_search.pdf"]]) ,autoReleaseDate: "Mar 16, 2025 04:00:00 EDT" },
         { cellNames: ["2/3", "Local Search", "R&N 4.0-4.1", "Notes", " "], cellNametoLinks: new Map<string,string>([["Notes", "lecture_notes/local_search.pdf"]]) ,autoReleaseDate: "Mar 16, 2025 04:00:00 EDT" },
@@ -77,42 +112,39 @@ export class Lectures extends React.Component<LectureProps> {
         { cellNames: ["4/21", "Special Topics", "R&N Chapter 3.5.2-3.7", "Notes", " "], cellNametoLinks: new Map<string,string>([["Notes", "lecture_notes/informed_search.pdf"],["Pathfinding Visualizations", "https://qiao.github.io/PathFinding.js/visual/"]]),autoReleaseDate: "Mar 16, 2025 04:00:00 EDT"  },
         { cellNames: ["4/23", "Special Topics", "R&N Chapter 3.5.2-3.7", "Notes", " "], cellNametoLinks: new Map<string,string>([["Notes", "lecture_notes/informed_search.pdf"],["Pathfinding Visualizations", "https://qiao.github.io/PathFinding.js/visual/"]]) ,autoReleaseDate: "Mar 16, 2025 04:00:00 EDT" },
         { cellNames: ["4/25", "Special Topics", "R&N Chapter 3.5.2-3.7", "Notes", " "], cellNametoLinks: new Map<string,string>([["Notes", "lecture_notes/informed_search.pdf"],["Pathfinding Visualizations", "https://qiao.github.io/PathFinding.js/visual/"]]) ,autoReleaseDate: "Mar 16, 2025 04:00:00 EDT" },
-        
+
         */
-       
-    ];
-    
+	];
 
-    render() {
-        // Modify the cellNametoLinks map based on the release status
-        const modifiedRows: RowOptions[] = this.myRows.map(row => {
-            if (!shouldRelease(row)) {
-                const updatedLinks = new Map<string, string>();
+	render() {
+		// Modify the cellNametoLinks map based on the release status
+		const modifiedRows: RowOptions[] = this.myRows.map((row) => {
+			if (!shouldRelease(row)) {
+				const updatedLinks = new Map<string, string>();
 
-                // Iterate over the original links and replace or remove them if necessary
-                row.cellNametoLinks.forEach((value, key) => {
-                    updatedLinks.set(key, "");
-                });
+				// Iterate over the original links and replace or remove them if necessary
+				row.cellNametoLinks.forEach((value, key) => {
+					updatedLinks.set(key, "");
+				});
 
-                return {
-                    ...row,
-                    cellNametoLinks: updatedLinks,
-                };
-            }
-            return row;
-        });
+				return {
+					...row,
+					cellNametoLinks: updatedLinks,
+				};
+			}
+			return row;
+		});
 
-        return (
-
-            <section id='lectures' className="anchor">
-                <div className= 'content-container'>
-                    <h2>Lectures</h2>
-                    <p className="mb-5">See also the <a href="#hours">course calendar</a>.</p>
-                    <TableSection header={this.myHeader} rows={modifiedRows} />
-                </div>
-
-            </section>
-
-        );
-    }
+		return (
+			<section id="lectures" className="anchor">
+				<div className="content-container">
+					<h2>Lectures</h2>
+					<p className="mb-5">
+						See also the <a href="#hours">course calendar</a>.
+					</p>
+					<TableSection header={this.myHeader} rows={modifiedRows} />
+				</div>
+			</section>
+		);
+	}
 }
