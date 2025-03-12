f915eda0b4d472e3f42c838f666bd03e16b1315e
Shivshankar Prasad <prasadshivshankar5@gmail.com>
2025-03-12 13:10:16 -0400

    hw 7

--- src/Components/Assignments/Assignments.tsx
+++ src/Components/Assignments/Assignments.tsx
@@ -98,9 +98,9 @@ export class Assignments extends React.Component<AssignmentProps> {
 		{
 			cellNames: ["Homework 7: Neural Networks", "3/12", "3/18"],
 			cellNametoLinks: new Map<string, string>([
-				["Homework 7: Neural Networks", "https://hackmd.io/@cs410/BkAJXzKKA"],
+				["Homework 7: Neural Networks", "https://hackmd.io/@cs410/BJKVPcOj1g"],
 			]),
-			autoReleaseDate: "Mar 12, 2026 14:00:00 EDT",
+			autoReleaseDate: "Mar 12, 2025 14:00:00 EDT",
 		},
 		{
 			cellNames: ["Homework 8: MDPs and Reinforcement Learning", "3/19", "4/1"],
