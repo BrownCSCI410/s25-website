70b5799d2d58c27c9c29569e3735b032be1ec512
Eric <eric_ewing@brown.edu>
2025-05-04 03:12:04 -0400

    update

---
+++ update_results.sh
@@ -0,0 +1,7 @@
+cd ~/s25_Tournament_Runner/submissions/
+python calculate_elo.py
+cp tournament_elo_rankings.json ~/s25-website/public/results.json
+cd ~/s25-website
+git add public/results.json
+git commit -m "update"
+git push
